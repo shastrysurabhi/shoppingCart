e540528a30b56095b7ac81f79c132005fcf3c4a5
surabhi <surabhi.shastry21@gmail.com>
2019-04-09 14:49:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import {JSON_CONFIG_FILENAME} from "tslint/lib/configuration";
+import {ROUTER_PROVIDERS} from "@angular/router/src/router_module";
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-shopping-cart',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Happy Shopping';
+  cart = [];
+  boxes = ['Chocolate', 'Cookies', 'Cakes', 'Ice Cream'];
+
+  selectedItem(item) {
+    let cartObject = {};
+    if (this.cart.length === 0) {
+      // first time adding into the cart
+      cartObject[item] = 1;
+      this.cart.push(cartObject);
+    } else {
+      console.log('inside else 1');
+      this.cart.forEach(aKey => {
+          if (aKey[item] !== undefined) {
+            // already present increase the counter
+            aKey[item] = aKey[item] + 1;
+          } else {
+            // not present in the cart, add new
+            cartObject[item] = 1;
+            this.cart.push(cartObject);
+          }
+      });
+    }
+    console.log('cart', this.cart);
+/*    localStorage.cartItems = JSON.stringify(this.cart);*/
+    alert(item + ' is added to Cart');
+  }
+
+  addNewItem(item) {
+    if (this.boxes.indexOf(item) !== -1) {
+      // already present
+      alert(item + ' is already present');
+    } else {
+      // new item add
+      this.boxes.push(item);
+      alert(item + ' is added');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgGridModule } from  'angular2-grid';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
